0c0f26781fc289d30ef88d22d64c6ed740d563c5
faraaryaresta <faraaryaresta@gmail.com>
2023-04-02 01:11:18 +0700

    add jawaban no 3

---
+++ soal_03.js
@@ -0,0 +1,8 @@
+// Pengulangan dari 0 sampai 20
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} adalah bilangan genap`);
+    } else {
+        console.log(`${i} adalah bilangan ganjil`);
+    }
+}
