eb2f4ce1d18684fc222a87a54ff7936928ba71c3
faraaryaresta <faraaryaresta@gmail.com>
2023-04-02 01:13:04 +0700

    add jawaban no 5

---
+++ soal_05.js
@@ -0,0 +1,11 @@
+let jawabanBenar  = "Impact Byte";
+let hasil = null;
+let konfirmasi = "Sebutkan kepanjangan dari nama IB?";
+
+while (hasil !== jawabanBenar ) {
+    hasil = prompt("Sebutkan kepanjangan dari nama IB?");
+} 
+alert("Selamat jawaban benar!");
+    
+ 
+
